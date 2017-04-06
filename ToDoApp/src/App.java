ec4d9d24346219199ce6ae538701ee0bc5681b72
Noemi Kecskes <nokecskes@gmail.com>
2017-04-06 16:28:13 +0200

    add method to be able to add new task to list

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -11,38 +11,22 @@ public class App {
 
   public static void main(String[] args) {
 
-    if (args.length == 0) {
-      System.out.println("Java Todo application");
-      System.out.println("=======================");
-      System.out.println();
-      System.out.println("Command line arguments:");
-      System.out.println("\t-l\tLists all the tasks");
-      System.out.println("\t-a\tAdds a new task");
-      System.out.println("\t-r\tRemoves an task");
-      System.out.println("\t-c\tCompletes an task");
+    ArgumentHandler handler = new ArgumentHandler(args);
+    ToDoList myToDoList = new ToDoList();
+
+    if (handler.noArgument()) {
+      myToDoList.printUsage();
     }
 
-    if (args.length > 0 && args[0].startsWith("-") && args[0].contains("l")) {
-      try {
-        Path myListPath = Paths.get("mylist.txt");
-        List<String> lines = Files.readAllLines(myListPath);
-        for (int i = 0; i < lines.size(); i++) {
-          System.out.println((i + 1) + " - " + lines.get(i));
-        }
-      } catch(Exception e){
-      System.out.println("error");
+    if (handler.contains("l")) {
+      myToDoList.listTasks();
     }
 
-
+    if (handler.contains("a")) {
+      myToDoList.append(args);
+    }
   }
-
-
-
-    /*When the application is ran with -l argument
-    Then it should print the tasks that are stored in the file
-    And it should add numbers before each*/
-
 }
-}
+
 
 
--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,6 +1,54 @@
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Connor on 2017.04.06..
  */
 public class ToDoList {
 
+  public void printUsage() {
+    System.out.println("Java Todo application");
+    System.out.println("=======================");
+    System.out.println();
+    System.out.println("Command line arguments:");
+    System.out.println("\t-l\tLists all the tasks");
+    System.out.println("\t-a\tAdds a new task");
+    System.out.println("\t-r\tRemoves an task");
+    System.out.println("\t-c\tCompletes an task");
+  }
+
+  public void listTasks() {
+    try {
+      Path myListPath = Paths.get("mylist.txt");
+      List<String> lines = Files.readAllLines(myListPath);
+      if (lines.size() == 0) {
+        System.out.println("No todos for today! :)");
+      } else {
+        for (int i = 0; i < lines.size(); i++) {
+          System.out.println((i + 1) + " - " + lines.get(i));
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("error");
+    }
+  }
+
+  public void append(String[] arguments) {
+    try {
+      Path myListPath = Paths.get("mylist.txt");
+      List<String> lines = Files.readAllLines(myListPath);
+      for (int i = 0; i < (arguments.length - 1); i++) {
+        if (arguments[i].equals("-a")) {
+          lines.add(arguments[i + 1]);
+        }
+      }
+      Files.write(myListPath, lines);
+    } catch (Exception e) {
+      System.out.println("No new task to add.");
+    }
+  }
 }
